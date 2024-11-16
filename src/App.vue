623fe21c115b0226d34602e13163162ea2240e99
MrGoblings <mrgoblings@gmail.com>
2024-11-16 14:45:44 +0200

    add: scrolling with the space bar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref, Ref, onMounted, nextTick, onUnmounted } from "vue";
+import Page from "./components/Page.vue";
+
+const pages: Ref<(InstanceType<typeof Page> | null)[]> = ref<HTMLElement[]>([]);
+const scrollToPage = (pageIndex: number) => {
+  const pageElement = pages.value[pageIndex];
+  if (pageElement && pageElement.$el instanceof HTMLElement) {
+    pageElement.$el.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+};
+
+const currentPage = ref(2);
+
+const handleKeydown = (event: KeyboardEvent) => {
+  if (event.code === "Space") {
+    currentPage.value = (currentPage.value + 1) % pages.value.length;
+    scrollToPage(currentPage.value);
+  }
+};
+
+onMounted(async () => {
+  await nextTick();
+  scrollToPage(currentPage.value);
+  window.addEventListener("keydown", handleKeydown);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", handleKeydown);
+});
 </script>
 
 <template>
-  <HelloWorld />
+  <Page v-for="i in 5" :key="i" :id="'page-' + i" ref="pages">
+    <div class="border-l-4 w-full h-full flex justify-center items-center">
+      <h1 class="font-bold text-6xl">Page {{ i }}</h1>
+    </div>
+  </Page>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/Page.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+defineProps<{
+  id: string;
+}>();
+</script>
+
+<template>
+  <div :id="id" class="min-w-full min-h-screen">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped></style>
